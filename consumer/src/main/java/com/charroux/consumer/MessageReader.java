0e20801fc790f7737f7405400fa8c029c7ccc66d
charroux <benoit.charroux@gmail.com>
2023-12-19 16:53:31 +0100

    consumer

---
+++ consumer/src/main/java/com/charroux/consumer/MessageReader.java
@@ -0,0 +1,32 @@
+package com.charroux.consumer;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serde;
+
+import java.util.Arrays;
+
+@Component
+public class MessageReader {
+
+    private static final Serde<String> STRING_SERDE = Serdes.String();
+
+    @Autowired
+    void buildPipeline(StreamsBuilder streamsBuilder) {
+        KStream<String, String> messageStream = streamsBuilder
+                .stream("my-topic", Consumed.with(STRING_SERDE, STRING_SERDE));
+
+        KTable<String, Long> wordCounts = messageStream
+                .mapValues((ValueMapper<String, String>) String::toLowerCase)
+                .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
+                .count();
+
+        String n = wordCounts.toStream().toString();
+        System.out.println("Received: " + n);
+    }
+
+}
